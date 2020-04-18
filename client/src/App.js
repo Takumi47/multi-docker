b87329be71bedfa1004011009907fdc3f0ebe986
Xander <Not Set>
2020-04-19 00:33:45 +0800

    web title change

--- client/src/App.js
+++ client/src/App.js
@@ -7,25 +7,11 @@ import Fib from './Fib';
 
 function App() {
   return (
-    // <Router>
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <h1 className="App-title">Fib Calculator UPDATED!</h1>
-    //       <Link to="/">Home</Link>
-    //       <Link to="/otherpage">Other Page</Link>
-    //     </header>
-    //     <div>
-    //         <Route exact path="/" component={Fib}/>
-    //         <Route path="/otherpage" component={OtherPage}/>
-    //     </div>
-    //   </div>
-    // </Router>
     <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Fib Calculator</h1>
+        <h1>🎲Fib Calculator🎲</h1>
         <Link to="/">Home</Link>
         <Link to="/otherpage">Other Page</Link>
         <div>
